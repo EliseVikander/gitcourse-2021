c6275cdb56fbc4f72d397e1769a437ef904ddfe7
EliseVikander <elise.vikander@hm.com>
2021-02-02 14:27:45 +0100

    Added clean code

--- src/Main.java
+++ src/Main.java
@@ -12,19 +12,6 @@ public class Main {
 
 
     }
-/*
-        public static String findShortestString(List<String> movieList) {
-            String shortest = null;
-            for (String current : movieList) {
-                if (shortest == null) { // first iteration
-                    shortest = current;
-                    continue;
-                }
-                int comparisonResult = comp.compare(shortest, current);
-                // TODO: your task; update shortest, depending on comparisonResult
-            }
-            return shortest;
-        }*/
 
 
     private static String findByString(List<String> movieList, String str) {
